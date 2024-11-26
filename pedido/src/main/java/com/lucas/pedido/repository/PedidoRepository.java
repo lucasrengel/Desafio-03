abbbe8f9f8b69aab597f375eb9e3ab4663cb80eb
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-11-26 10:39:46 -0300

    ✨ feat: pedido repository

---
+++ pedido/src/main/java/com/lucas/pedido/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.lucas.pedido.repository;
+
+import com.lucas.pedido.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
